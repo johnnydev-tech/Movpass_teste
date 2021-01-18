e76ec24a801bd98bbeab5efb461e6c5ee7c68bda
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-01-18 00:40:22 -0300

    Changed ButtonCustom and Create view Home

--- lib/components/buttonCustom.dart
+++ lib/components/buttonCustom.dart
@@ -6,21 +6,25 @@ class ButtonCustom extends StatelessWidget {
   final Color textColor;
   final VoidCallback onPressed;
 
-  ButtonCustom(
-      {@required this.texto,
-      this.bgColor = Colors.white,
-      this.onPressed,
-      this.textColor});
+  ButtonCustom({
+    @required this.texto,
+    this.bgColor,
+    this.onPressed,
+    this.textColor = Colors.white,
+  });
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
+      padding: EdgeInsets.symmetric(vertical: 14),
       child: Text(
         this.texto,
         style: TextStyle(
+          fontSize: 16,
           color: this.textColor,
         ),
       ),
       color: this.bgColor,
+      splashColor: Colors.white24,
       onPressed: this.onPressed,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
     );
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movpass_test/components/buttonCustom.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +9,35 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset("assets/logo1.webp"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ButtonCustom(
+                  texto: "Treinadores",
+                  bgColor: Color(0xffE9A945),
+                  onPressed: () {},
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                ButtonCustom(
+                  texto: "Modalidades",
+                  bgColor: Color(0xff242A2C),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
