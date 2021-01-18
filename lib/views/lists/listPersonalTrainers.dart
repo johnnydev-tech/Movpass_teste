27e3aa07efd2e8dfb6a70142c39c8764dcd7319c
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-01-18 16:12:32 -0300

    Create ListTrainer with API

--- lib/views/lists/listPersonalTrainers.dart
+++ lib/views/lists/listPersonalTrainers.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movpass_test/controller/trainerController.dart';
+import 'package:movpass_test/model/trainer.dart';
 
 class ListTrainers extends StatefulWidget {
   @override
@@ -6,6 +8,12 @@ class ListTrainers extends StatefulWidget {
 }
 
 class _ListTrainersState extends State<ListTrainers> {
+  TrainerAPI api = TrainerAPI();
+
+  _listTrainer() {
+    return api.getAll();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +22,54 @@ class _ListTrainersState extends State<ListTrainers> {
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
+        child: FutureBuilder<List<Trainer>>(
+          future: _listTrainer(),
+          // ignore: missing_return
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor:
+                        new AlwaysStoppedAnimation<Color>(Color(0xffE9A945)),
+                  ),
+                );
+                break;
+              case ConnectionState.active:
+              case ConnectionState.done:
+                if (snapshot.hasError) {
+                  print("Erro ao carregar lista" + snapshot.error.toString());
+                  return Center(child: Text("Error"),);
+                } else {
+                  return Container(
+                    padding: EdgeInsets.all(5),
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        List<Trainer> lista = snapshot.data;
+                        Trainer trainer = lista[index];
+
+                        return Padding(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: ListTile(
+                              trailing: Icon(Icons.arrow_forward_ios),
+                              title: Text(trainer.name ),
+                              subtitle: Text(trainer.cref),
+                              onTap: (){
+
+                              },
+                            ));
+                      },
+                    ),
+                  );
+                }
+                break;
+            }
+          },
+        ),
       ),
     );
   }
